e7d8255708e7aa470a99ff8b91f5d18eb541f34f
JamesFW <jamesfw@jw-Latitude-E6420.(none)>
2012-10-03 03:47:51 -0400

    Didn't add the status-reader.js file for some reason, adding it now

--- status-reader.js
+++ status-reader.js
@@ -1,7 +1,35 @@
-var http = require('http');
+var express = require('express');
+var xmpp = require('simple-xmpp');
 var fs = require('fs');
 
-http.createServer( function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(index);
-}).listen(80);
+fs.readFile('credentials.json','ASCII', function(err, data) {
+    if(err) throw err;
+    var creds = eval('('+data+')');
+    var user = creds.user;
+    var pwd = creds.pwd;
+    start(user,pwd);
+});
+
+
+xmpp.on('online', function() {
+    console.log('yo, im here');
+});
+
+xmpp.on('chat', function(from,message) {
+    xmpp.send(from, 'echo ' + message);
+});
+
+xmpp.on('error', function(err) {
+    console.error(err);
+});
+
+function start(user,pwd) {
+xmpp.connect({
+    jid:        user,
+    password:   pwd,
+    host:       'talk.google.com',
+    port:       5222
+});
+}
+
+

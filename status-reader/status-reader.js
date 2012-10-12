b5dd6b8d7ce2dfa03ad3e9d891a1a9f71013850d
JamesFW <jamesfw@jw-Latitude-E6420.(none)>
2012-10-12 04:22:18 -0400

    Parse stanzas, form timestamped output string, write to file

--- status-reader/status-reader.js
+++ status-reader/status-reader.js
@@ -2,9 +2,11 @@ var express = require('express');
 var xmpp = require('simple-xmpp');
 var fs = require('fs');
 var xmljson = require('libxmljs');
+//var diff = require('./lib/jsdiff.js');
 var vars = {};
 
-vars.data = fs.readFileSync('credentials.json','ASCII');
+vars.data = fs.readFileSync('credentials.json','ASCII'); //synchronous
+vars.stream = fs.createWriteStream('output.txt'); //fs.WriteStream
 var creds = eval('('+vars.data+')');
 vars.user = creds.user;
 vars.pwd = creds.pwd;
@@ -12,7 +14,6 @@ start(vars.user,vars.pwd);
 
 xmpp.on('online', function() {
     console.log('Connected to ' + vars.user);
-  //  probe(vars.user);
 
 });
 
@@ -30,20 +31,38 @@ xmpp.on('buddy',function(jid, state) {
 });
 
 xmpp.on('stanza', function(stanza) {
-    vars.json = xmljson.parseXml(stanza);
-    console.log(Date()
-    + vars.json.get('status') + ' '
-    + vars.json.get('show') + ' '
-    + vars.json.get('priority'));
-});
+    vars.json = xmljson.parseXmlString(stanza);
+    vars.stat = vars.json.get('//status');
+    vars.show = vars.json.get('//show');
+    vars.pri = vars.json.get('//priority');
+    if(vars.stat) {
+    vars.statstr = vars.stat.text();
+    }
+    if(vars.show) {
+    vars.showstr = vars.show.text();
+    }
+    if(vars.pri) {
+    vars.pristr = vars.pri.text();
+    }
+    
+    var logentry = ""
+     + Date() + ' '
+     + vars.statstr + ' '
+     + vars.showstr + ' '
+     + vars.pristr;
+    
+    vars.stream.write(logentry + '\n');
+    console.log(logentry);
+//    console.log(stanza + '\n');
+   });
 
 function start(user,pwd) {
     console.log('Connecting to ' + user);
-xmpp.connect({
-    jid:        user,
-    password:   pwd,
-    host:       'talk.google.com',
-    port:       5222
+    xmpp.connect({
+        jid:        user,
+        password:   pwd,
+        host:       'talk.google.com',
+        port:       5222
 });
 }
 

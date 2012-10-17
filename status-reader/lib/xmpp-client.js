53cbb187576c02b9486318096a0451235c888f8d
JamesFW <jamesfw@jw-Latitude-E6420.(none)>
2012-10-17 03:29:49 -0400

    Deleted some outdated dependencies

--- status-reader/lib/xmpp-client.js
+++
@@ -1,3 +0,0 @@
-exports.BasicClient = require('./xmpp-client/basic-client').BasicClient;
-exports.Client = require('./xmpp-client/client').Client;
-exports.Jid    = require('./xmpp-client/client').Jid;
